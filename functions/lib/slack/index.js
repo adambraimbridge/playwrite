815a9ec409548198764c30ba4e523da4ebf6cc56
Adam Braimbridge <adam.braimbridge@ft.com>
2020-04-29 16:42:19 +0100

    Slack webhook now correctly verifies.

--- functions/lib/slack/index.js
+++ functions/lib/slack/index.js
@@ -1,5 +1,29 @@
-const slack = () => {}
+/**
+ * @param {*} body The body of the request from Slack
+ */
+const getPayload = ({ body }) => {
+	const payload = body.payload ? body.payload : body
+	return JSON.parse(payload)
+}
+
+/**
+ * Route interactions and events based on Slack payload.
+ * Payload is different depending on what Slack sends.
+ *
+ * @param {*} request The data sent from Slack
+ */
+const handleSlackRequest = (request) => {
+	try {
+		const payload = getPayload(request)
+		console.log('handleSlackRequest ...')
+		console.log({ payload })
+	} catch (error) {
+		// @todo Catch any errors and trigger an "error" event that the app can subscribe to ..?
+		console.log(error.message)
+	}
+}
 
 module.exports = {
-	slack,
+	getPayload,
+	handleSlackRequest,
 }
--- functions/slack-public-webhook.js
+++ functions/slack-public-webhook.js
@@ -6,15 +6,25 @@
  * @see https://slack.dev/node-slack-sdk/web-api
  */
 
-const { slack } = require('./lib/slack')
+const { getPayload, handleSlackRequest } = require('./lib/slack')
 
-exports.handler = (payload) => {
-	console.log('Testing ...')
-	console.log({ payload })
+exports.handler = (request) => {
+	/**
+	 * Hand over to the slack router
+	 *
+	 * Do not await a response, because otherwise it will time out,
+	 * and Slack would keep resending the event.
+	 */
+	handleSlackRequest(request)
+
+	// Handle Slack challenges. See: https://api.slack.com/events/url_verification
+	const payload = getPayload(request)
+	const { type, challenge } = payload
+	const body = type === 'url_verification' && !!challenge ? challenge : 'ok'
 
 	// Always respond with OK
 	return {
 		statusCode: 200,
-		body: 'ok',
+		body,
 	}
 }
