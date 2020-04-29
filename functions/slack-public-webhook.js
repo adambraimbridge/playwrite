76ac8d9d670e47c9cec95105e3a966dadc477712
Adam Braimbridge <adam.braimbridge@ft.com>
2020-04-29 11:55:09 +0100

    Debugging payload

--- functions/slack-public-webhook.js
+++ functions/slack-public-webhook.js
@@ -1,20 +1,25 @@
+const { createEventAdapter } = require('@slack/events-api')
+
 /**
  * Slack Public Webhook
  *
  * This function is called from Slack.
- * @see https://slack.dev/bolt
+ * @see https://github.com/slackapi/node-slack-sdk
+ * @see https://slack.dev/node-slack-sdk/events-api
  */
 
 const controller = () => {
-	return {
-		statusCode: 200,
-		body: {
-			testing: true,
-		},
-	}
+	const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
+	const slackEvents = createEventAdapter(slackSigningSecret, {
+		includeBody: true,
+		includeHeaders: true,
+	})
+	return slackEvents
 }
 
-exports.handler = () => {
+exports.handler = (payload) => {
 	console.log('Testing ...')
-	return controller()
+	console.log({ payload })
+	const foo = controller()
+	console.log({ foo })
 }
