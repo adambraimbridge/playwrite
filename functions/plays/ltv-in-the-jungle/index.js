c81a852bdd9954e6215d75dbe9fe7520db24bb78
Adam Braimbridge <adam.braimbridge@ft.com>
2020-05-04 02:33:03 +0100

    In the Jungle

---
+++ functions/plays/ltv-in-the-jungle/index.js
@@ -0,0 +1,177 @@
+const view = {
+	blocks: [
+		{
+			type: 'section',
+			fields: [
+				{
+					type: 'mrkdwn',
+					text: '*Top Players*\n<fakelink.toUrl.com|Winston Churchill>\n<fakelink.toUrl.com|Nicole Kidman>\n<fakelink.toUrl.com|Phar Lap>',
+				},
+				{
+					type: 'mrkdwn',
+					text: '*Top Plays*\n<fakelink.toUrl.com|LTV in the Jungle>\n<fakelink.toUrl.com|Journalist or Pugalist?>\n<fakelink.toUrl.com|Match the Headline>',
+				},
+			],
+		},
+		{
+			type: 'context',
+			elements: [
+				{
+					type: 'image',
+					image_url: 'https://api.slack.com/img/blocks/bkb_template_images/placeholder.png',
+					alt_text: 'placeholder',
+				},
+			],
+		},
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: '*Now Showing*',
+			},
+			accessory: {
+				type: 'overflow',
+				options: [
+					{
+						text: {
+							type: 'plain_text',
+							text: 'Newest',
+							emoji: true,
+						},
+						value: 'value-0',
+					},
+					{
+						text: {
+							type: 'plain_text',
+							text: 'Most popular',
+							emoji: true,
+						},
+						value: 'value-1',
+					},
+					{
+						text: {
+							type: 'plain_text',
+							text: 'Trending',
+							emoji: true,
+						},
+						value: 'value-2',
+					},
+				],
+			},
+		},
+		{
+			type: 'divider',
+		},
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: '*LTV in the Jungle* :star: :star: :star: :star:',
+			},
+		},
+		{
+			type: 'context',
+			elements: [
+				{
+					type: 'image',
+					image_url: 'https://api.slack.com/img/blocks/bkb_template_images/profile_3.png',
+					alt_text: 'Dwight Schrute',
+				},
+				{
+					type: 'mrkdwn',
+					text: '*Dwight Schrute*',
+				},
+			],
+		},
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: 'A fast-paced action adventure pitting you agains the wild.',
+			},
+		},
+		{
+			type: 'actions',
+			elements: [
+				{
+					type: 'button',
+					text: {
+						type: 'plain_text',
+						text: 'Play',
+						emoji: true,
+					},
+					style: 'primary',
+					value: 'approve',
+				},
+				{
+					type: 'button',
+					text: {
+						type: 'plain_text',
+						text: 'View Details',
+						emoji: true,
+					},
+					value: 'details',
+				},
+			],
+		},
+		{
+			type: 'divider',
+		},
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: '*Journalist or Pugalist?* :star: :star: :star:',
+			},
+		},
+		{
+			type: 'context',
+			elements: [
+				{
+					type: 'image',
+					image_url: 'https://api.slack.com/img/blocks/bkb_template_images/profile_2.png',
+					alt_text: 'Pam Beasely',
+				},
+				{
+					type: 'mrkdwn',
+					text: '*Pam Beasely*',
+				},
+			],
+		},
+		{
+			type: 'section',
+			text: {
+				type: 'mrkdwn',
+				text: 'A fast-paced action adventure pitting you against the wild.',
+			},
+		},
+		{
+			type: 'actions',
+			elements: [
+				{
+					type: 'button',
+					text: {
+						type: 'plain_text',
+						text: 'Play',
+						emoji: true,
+					},
+					style: 'primary',
+					value: 'approve',
+				},
+				{
+					type: 'button',
+					text: {
+						type: 'plain_text',
+						text: 'View Details',
+						emoji: true,
+					},
+					value: 'details',
+				},
+			],
+		},
+	],
+}
+
+module.exports = {
+	view,
+}
